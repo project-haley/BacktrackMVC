485d95a36e45d353463a0978f0fc2bb38e436264
project-haley <60005425+project-haley@users.noreply.github.com>
2021-09-05 03:33:04 -0500

    Initial commit

---
+++ Backtrack/Models/Palindrome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backtrack.Models
+{
+    public class Palindrome
+    {
+        public string InputString { get; set; }
+        public string ReverseString { get; set; }
+        public bool IsPalindrome { get; set; }
+        public string Message { get; set; }
+
+
+    }
+}
